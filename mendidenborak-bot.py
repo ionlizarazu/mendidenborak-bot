40c7248a56f7d4e233112352f1a2b8148c445c87
ionlizarazu <ilizarazu@codesyntax.com>
2021-12-07 18:24:19 +0100

    principal funcionality done

--- mendidenborak-bot.py
+++ mendidenborak-bot.py
@@ -5,8 +5,6 @@ from mendidenborak import MendiDenborak
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
 )
 from telegram.ext import (
     CallbackQueryHandler,
@@ -60,19 +58,10 @@ class Ibilbidea:
             return -1, "Luzeera ez da zenbaki osoa"
 
 
-def startingMenu():
-    return InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    "Hasi ibilbidearekin", callback_data="hasiIbilbidea"
-                )
-            ],
-        ]
-    )
-
-
 def startBot(update, context, update_message=False, language=False):
+    dp.remove_handler(lonHandler)
+    dp.remove_handler(posHandler)
+    dp.remove_handler(negHandler)
     if update.message:
         message = update.message
         user = message.from_user
@@ -89,38 +78,142 @@ def startBot(update, context, update_message=False, language=False):
     )
 
 
-def hasiIbilbidea(
+def startingMenu():
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "Hasi ibilbidearekin", callback_data="aukeratuBidea"
+                )
+            ],
+        ]
+    )
+
+
+def aukeratuBidea(
     update,
     context,
-    update_message=False,
-    language=False,
 ):
+    ibilbidea = Ibilbidea()
+    context.bot_data["ibilbidea"] = ibilbidea
     if update.message:
         message = update.message
     else:
         message = update.callback_query.message
 
+    update.callback_query.edit_message_text(text="Updating text")
+    context.bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+
     context.bot.send_message(
         chat_id=message.chat.id,
         text=f"Denbora kalkulatzeko, aurrena aukeratu bide mota nagusia:",
-        reply_markup=ibilbideaMenu(),
+        reply_markup=bideaMenu(),
     )
 
 
-def ibilbideaMenu():
+def bideaMenu():
     return InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("pista", callback_data="Pista")],
-            [InlineKeyboardButton("bidexka", callback_data="Bidexka")],
-            [InlineKeyboardButton("bidexka-zaila", callback_data="BidexkaZaila")],
-            [InlineKeyboardButton("bidez-kanpo", callback_data="BidezKanpo")],
+            [InlineKeyboardButton("Pista", callback_data="pista")],
+            [InlineKeyboardButton("Bidexka", callback_data="bidexka")],
+            [InlineKeyboardButton("Bidexka zaila", callback_data="bidexka-zaila")],
+            [InlineKeyboardButton("Bidez-kanpo", callback_data="bidez-kanpo")],
         ]
+    )
+
+
+def aukeratuLuzeera(update, context):
+    if update.message:
+        message = update.message
+    else:
+        message = update.callback_query.message
+
+    context.bot_data.get("ibilbidea").set_bidea(update.callback_query.data)
+    ibilbidea = context.bot_data["ibilbidea"]
+    dp.remove_handler(posHandler)
+    dp.remove_handler(negHandler)
+    dp.add_handler(lonHandler)
+
+    context.bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Definitutako ibilbidea:\n- Bide mota: {ibilbidea.bidea}\n- Luzeera (m): {ibilbidea.luzeera}\n- Denibel positiboa: {ibilbidea.positiboa}\n- Desnibel negatiboa: {ibilbidea.negatiboa}\n\nIdatzi bidearen LUZEERA METROTAN:",
+        # reply_markup=ibilbideaMenu(),
+    )
+
+
+def aukeratuPositiboa(update, context):
+    if update.message:
+        message = update.message
+    else:
+        message = update.callback_query.message
+
+    context.bot_data.get("ibilbidea").set_luzeera(update.message.text)
+    ibilbidea = context.bot_data["ibilbidea"]
+    dp.remove_handler(lonHandler)
+    dp.remove_handler(negHandler)
+    dp.add_handler(posHandler)
+
+    context.bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Definitutako ibilbidea:\n- Bide mota: {ibilbidea.bidea}\n- Luzeera (m): {ibilbidea.luzeera}\n- Denibel positiboa: {ibilbidea.positiboa}\n- Desnibel negatiboa: {ibilbidea.negatiboa}\n\nIdatzi bidearen DESNIBEL POSITIBOA metrotan:",
+        # reply_markup=ibilbideaMenu(),
+    )
+
+
+def aukeratuNegatiboa(update, context):
+    if update.message:
+        message = update.message
+    else:
+        message = update.callback_query.message
+
+    context.bot_data.get("ibilbidea").set_positiboa(update.message.text)
+    ibilbidea = context.bot_data["ibilbidea"]
+    dp.remove_handler(lonHandler)
+    dp.remove_handler(posHandler)
+    dp.add_handler(negHandler)
+
+    context.bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Definitutako ibilbidea:\n- Bide mota: {ibilbidea.bidea}\n- Luzeera (m): {ibilbidea.luzeera}\n- Denibel positiboa: {ibilbidea.positiboa}\n- Desnibel negatiboa: {ibilbidea.negatiboa}\n\nIdatzi bidearen DESNIBEL NEGATIBOA metrotan:",
+        # reply_markup=ibilbideaMenu(),
+    )
+
+
+def kalkulatuDenbora(update, context):
+    if update.message:
+        message = update.message
+    else:
+        message = update.callback_query.message
+
+    context.bot_data.get("ibilbidea").set_negatiboa(update.message.text)
+    ibilbidea = context.bot_data["ibilbidea"]
+    dp.remove_handler(lonHandler)
+    dp.remove_handler(posHandler)
+    dp.add_handler(negHandler)
+    md = MendiDenborak()
+    denbora_raw = md.kalkulatuDenbora(
+        ibilbidea.bidea, ibilbidea.luzeera, ibilbidea.positiboa, ibilbidea.negatiboa
+    )
+    ordu = int(denbora_raw)
+    minutu = int((denbora_raw * 60) % 60)
+
+    context.bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Definitutako ibilbidea:\n- Bide mota: {ibilbidea.bidea}\n- Luzeera (m): {ibilbidea.luzeera}\n- Denibel positiboa: {ibilbidea.positiboa}\n- Desnibel negatiboa: {ibilbidea.negatiboa}",
+        # reply_markup=ibilbideaMenu(),
+    )
+
+    context.bot.send_message(
+        chat_id=message.chat.id,
+        text=f"Ibilbide hau egiteko kalkulatutako denbora:\n\n{ordu} ordu eta {minutu} minutu.",
+        reply_markup=startingMenu(),
     )
 
 
 def aukerak(update, context):
     query = update.callback_query
     query.answer()
+
     theFunction = globals()[query.data]
     theFunction(update, context)
 
@@ -130,20 +223,31 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+lonHandler = MessageHandler(Filters.text & ~Filters.command, aukeratuPositiboa)
+posHandler = MessageHandler(Filters.text & ~Filters.command, aukeratuNegatiboa)
+negHandler = MessageHandler(Filters.text & ~Filters.command, kalkulatuDenbora)
+
+
 def main():
     """Start the bot."""
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", startBot))
     dp.add_handler(CommandHandler("hasi", startBot))
+    dp.add_handler(CallbackQueryHandler(aukeratuBidea, pattern="aukeratuBidea"))
+    dp.add_handler(
+        CallbackQueryHandler(
+            aukeratuLuzeera, pattern="(pista|bidexka|bidexka-zaila|bidez-kanpo)"
+        )
+    )
     dp.add_handler(CallbackQueryHandler(aukerak))
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
+    # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-
     updater.bot.setWebhook("https://mendidenborak-bot.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
