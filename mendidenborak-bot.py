1e51de1e71b00317b180fb8f50466543a4059cb4
ionlizarazu <ilizarazu@codesyntax.com>
2021-12-25 11:46:18 +0100

    webhook-a gaitu

--- mendidenborak-bot.py
+++ mendidenborak-bot.py
@@ -251,9 +251,9 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    # updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
-    # updater.bot.setWebhook("https://mendidenborak-bot.herokuapp.com/" + TOKEN)
+    # updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook("https://mendidenborak-bot.herokuapp.com/" + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
